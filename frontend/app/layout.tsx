9aeb382ef446d23fe5d895c696824538b6881c47
martin machiebe <martinmachiebe21@gmail.com>
2025-01-19 19:48:35 +0100

    updates

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Footer from "@/components/layout/footer";
 import Nav from "@/components/layout/nav";
 import StarknetProvider from "@/components/starknet-provider";
+import Script from "next/script";
 
 const Jersey10 = Jersey_10({
   subsets: ["latin"],
@@ -11,14 +12,15 @@ const Jersey10 = Jersey_10({
   variable: "--font-jersey-10",
 });
 
-const  WorkSans = Work_Sans({
+const WorkSans = Work_Sans({
   variable: "--font-work-sans",
   subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
   title: "PrediFI - Onchain Price Prediction",
-  description: "Prediction Protocol built on starknet, predict various outcomes across various fields",
+  description:
+    "Prediction Protocol built on starknet, predict various outcomes across various fields",
 };
 
 export default function RootLayout({
@@ -28,12 +30,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {" "}
+        <Script src="https://telegram.org/js/telegram-web-app.js"></Script>
+      </head>
       <body
         className={`${Jersey10.variable} ${WorkSans.variable} antialiased px-5 md:px-10 xl:px-16 bg-[#1E1E1E] text-[#FFFFFF] font-work`}
       >
         <StarknetProvider>
           <Nav />
-          <section className="max-w-screen-[1500px] mx-auto mt-14 min-h-screen pb-14">{children}</section>
+          <section className="max-w-screen-[1500px] mx-auto mt-14 min-h-screen pb-14">
+            {children}
+          </section>
           <Footer />
         </StarknetProvider>
       </body>
